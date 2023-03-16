c32ed4894427371727fe28ee39f41f022b1ca51f
Harry <47954462+5ifty@users.noreply.github.com>
2023-03-16 11:51:16 +0000

    Add in for Loop to start iteration of Json.

--- scripts/conversion.py
+++ scripts/conversion.py
@@ -11,7 +11,7 @@ class SpreadsheetConverter():
         pass
 # This converter function allows for multiple processes
 # in converting the xlsx file into a readable json format which will then be processed into the DB
-    def converter():
+    def converter(self):
         preconversion = pandas.read_excel(r"C:/Users/user/Documents/DB_Encryption/input/spreadsheetData.xlsx", sheet_name='ClientData')
 
         conversionList = preconversion.to_json(orient='records') # This outputs a string in json format
@@ -20,11 +20,29 @@ class SpreadsheetConverter():
     # This is dumping the data into a .json format
     # Due to error handling I've decided to check if the path is there or not
     # If it is there, we will continue, else we will make the file to dump, to stop any PathSpec Errors.
-        Path = r'C:/Users/user/Documents/DB_Encryption/output/convertedData.json'
-        with open(r'C:/Users/user/Documents/DB_Encryption/output/convertedData.json', 'w+') as convertedData:
+        self.Path = r'C:/Users/user/Documents/DB_Encryption/output/convertedData.json'
+        with open(self.Path, 'w+') as convertedData:
             json.dump(conversionDict, convertedData)
+        
+    def iterateJson(self):
+        with open(self.Path, 'r') as toPrettify:
+            jsonify = json.load(toPrettify)
+            for first_name in range(len(jsonify)):
+                jsonify[first_name]['first_name']
+            for last_name in range(len(jsonify)):
+                jsonify[last_name]['last_name']
+            for company in range(len(jsonify)):
+                jsonify[company]['Company']
+            for email in range(len(jsonify)):
+                jsonify[email]['email']
+            
 
 
+        
+
+
+        
 if __name__ == '__main__':
-    SpreadsheetConverter()
-    SpreadsheetConverter.converter()
+    SSC = SpreadsheetConverter()
+    SSC.converter()
+    SSC.iterateJson()
