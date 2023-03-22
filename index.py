5347e8ed19fe17b46d132ff7eca31908ea1a525c
Harry <5iftyy666@gmail.com>
2023-03-22 23:57:53 +0000

    Fix bugs -Final

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from scripts.conversion import SpreadsheetConverter
 from scripts.encryption import FileEncryption
 from scripts.decryption import Decryption
 from Database import DB
+import json
 
 '''
 This is adding all of the components in ../scripts/ and processing each stage before we move towards the DB
@@ -29,42 +30,58 @@ class Main():
         print('2: View Encryption Key')
         print('3. Query Database, this is based off the customers first name.')
         print('4.View Raw Data')
+        print('You will be brought back here after every action, everything is autosaved so exit the CMD when you are ready too.')
 
-        userInput = int(input('Please type in the corresponding number to the action you wish to complete'))
+        userInput = int(input('Please type in the corresponding number to the action you wish to complete\n'))
         if userInput == 1:
-            userInput = input('Please enter the security Key to access this')
+            userInput = input('Please enter the security Key to access this\n')
             if userInput != password:
-                print('Incorrect Security Key, please try again')
-                Main.cmd()
+                print('Incorrect Security Key, please try again\n')
+                self.cmd()
             else:
-                Main.master()
+                self.master()
+                self.cmd()
         if userInput == 2:
-            userInput = input('Please enter the security Key to access this')
+            userInput = input('Please enter the security Key to access this\n')
             if userInput != password:
                 print('Incorrect Security Key, please try again')
-                Main.cmd()
+                self.cmd()
             else:
-                with open(r'C:\Users\user\Documents\DB_Encryption\output\encryptionKey.json', 'r') as encryptionKey:
-                    for key in encryptionKey:
-                        print(key['key'])
+                with open(r'C:\Users\oem\Documents\DB_Encryption\output\encryptionKey.json', 'r') as encryptionKeyJson:
+                    encryptionKey = json.load(encryptionKeyJson)
+                    print(encryptionKey['key'])
+                    self.cmd()
         if userInput == 3:
-            userInput = input('Please enter the security Key to access this')
+            userInput = input('Please enter the security Key to access this\n')
             if userInput != password:
-                print('Incorrect Security Key, please try again')
-                Main.cmd()
+                print('Incorrect Security Key, please try again\n')
+                self.cmd()
             else:
                 db = DB()
-                name = input("Please enter the customer's first name:")
+                name = input("Please enter the customer's first name:\n")
                 dbQuery = db.queryDatabase(name)
                 print(dbQuery)
+                self.cmd()
         if userInput == 4:
-            userInput = input('Please enter the security Key to access this')
+            userInput = input('Please enter the security Key to access this\n')
             if userInput != password:
-                print('Incorrect Security Key, please try again')
-                Main.cmd()
+                print('Incorrect Security Key, please try again\n')
+                self.cmd()
             else:
-                with open(r'C:\Users\user\Documents\DB_Encryption\output\CSVData.csv', 'r') as rawData:
-                    print(rawData)
+                with open(r'C:\Users\oem\Documents\DB_Encryption\output\decryptedData.json', 'r') as rawData:
+                    iterateJson = json.load(rawData)
+                    for first_name in iterateJson:
+                        openDict = iterateJson[first_name]
+                        firstName = openDict['first_name']
+                        lastName = openDict['last_name']
+                        company = openDict['Company']
+                        email = openDict['email']
+                        risk = openDict['Risk']
+                        contact = openDict['contact']
+                        status = openDict['Status']
+                        print(firstName, lastName, company, email, risk, contact, status)
+            
+            self.cmd()
     
 
 if __name__ == '__main__':
